61f9a73c6fc736148bd6c786490833f0eb4be1ed
tanguyen2020 <75058422+tanguyen2020@users.noreply.github.com>
2021-09-17 21:49:29 +0700

    Create TypeSearchMatch.cs

---
+++ CacheOrSearchEngine/ElasticSearch/TypeSearchMatch.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheOrSearchEngine.ElasticSearch
+{
+    public static class TypeSearchMatch
+    {
+        public const string PhrasePrefix = "phrase_prefix";
+    }
+}
