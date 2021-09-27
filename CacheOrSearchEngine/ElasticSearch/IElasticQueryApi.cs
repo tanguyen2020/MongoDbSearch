ebb6dc1339120da32227221cf4c2e99e77afcffa
tanguyen2020 <75058422+tanguyen2020@users.noreply.github.com>
2021-09-27 20:48:12 +0700

    Update IElasticQueryApi.cs

--- CacheOrSearchEngine/ElasticSearch/IElasticQueryApi.cs
+++ CacheOrSearchEngine/ElasticSearch/IElasticQueryApi.cs
@@ -10,6 +10,7 @@ namespace CacheOrSearchEngine.ElasticSearch
         Task CreateIndexAysnc<TDocument>(TDocument document, string indexName);
         Task<List<TDocument>> SearchAysnc<TDocument>(string query, string indexName);
         Task DeleteAsync<TDocument>(string query, string indexName);
+        Task DeleteAsync<TDocument>(string[] query, string indexName);
         Task DeleteIndexAsync(string indexName);
         Task DeleteAllIndexAsync();
     }
