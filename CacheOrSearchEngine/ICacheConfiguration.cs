08a27b7595ee158f7244860ef58deea5c9bc4297
tanguyen2020 <75058422+tanguyen2020@users.noreply.github.com>
2021-09-28 11:50:49 +0700

    Add files via upload

---
+++ CacheOrSearchEngine/ICacheConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Configuration settings cache
+/// </summary>
+namespace Caching.Core
+{
+    public interface ICacheConfiguration
+    {
+        /// <summary>
+        /// Atrribute confirm is use Caching
+        /// If EnableCache = false is turn off cache
+        /// If EnableCache = true is turn on cache
+        /// </summary>
+        bool EnableCache { get; }
+
+        /// <summary>
+        /// Endpoint Uri Azure CosmosDB
+        /// </summary>
+        string CosmosAccountEndpoint { get; }
+
+        /// <summary>
+        /// Primary key Azure CosmosDB
+        /// </summary>
+        string CosmosAccountKey { get; }
+
+        /// <summary>
+        /// DatabaseName on Azure CosmosDB
+        /// </summary>
+        string CosmosDataBaseName { get; }
+
+        /// <summary>
+        /// Container on Azure CosmosDB
+        /// </summary>
+        string CosmosCollection { get; }
+
+        List<DateTime> CMSReleasedDate { get;}
+        DateTime? FindPriodDate(DateTime dateTime);
+
+        /// <summary>
+        /// The system will automatically delete items based on the TTL value (in seconds) you provide, without needing a delete operation explicitly issued by a client application
+        /// </summary>
+        int? CacheTimeExpired { get; }
+    }
+}
