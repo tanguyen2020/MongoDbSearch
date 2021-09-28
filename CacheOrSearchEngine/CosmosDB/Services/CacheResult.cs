8389f95e94adaca7f7120b2d425f43607f3759ef
tanguyen2020 <75058422+tanguyen2020@users.noreply.github.com>
2021-09-28 14:13:16 +0700

    Create CacheResult.cs

---
+++ CacheOrSearchEngine/CosmosDB/Services/CacheResult.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching.Services
+{
+    public class CacheResult
+    {
+        [JsonProperty("id")]
+        public string key { get; }
+        public string Value { get; }
+
+        // Summary: 
+        // used to set expiration policy
+        // The system will automatically delete items based on the TTL value(in seconds) you provide, without needing a delete operation explicitly issued by a client application
+        [JsonProperty(PropertyName = "ttl", NullValueHandling = NullValueHandling.Ignore)]
+        public int TimeToLive { get; set; } = -1;
+
+        public CacheResult(string key, string value, int expiry = -1)
+        {
+            this.key = key;
+            this.Value = value;
+            TimeToLive = expiry == 0 ? -1 : expiry;
+        }
+    }
+}
